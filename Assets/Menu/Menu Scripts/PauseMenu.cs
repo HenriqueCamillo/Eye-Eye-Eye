661e84154296abec8b1dc434fd886684c37793e1
Raphael Zoega Cali Gomes <raphael.z.cali@gmail.com>
2020-05-22 01:18:23 -0300

    Still needs change in UI images /// Added Main menu and Pause menu /// Main menu needs new background /// Pause menu created as scene, but the 'PauseMenu' game object should be put inside each of the game's scenes (the scene itself is useless <place-holder>)

---
+++ Assets/Menu/Menu Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+	public GameObject pauseMenu;
+
+	void Update()
+	{
+		if(Input.GetKeyDown("escape"))
+		{
+			pauseMenu.SetActive(true);
+			Time.timeScale = 0f;
+		}
+	}
+
+	public void Unpause()
+	{
+		Time.timeScale = 1f;
+	}
+}
